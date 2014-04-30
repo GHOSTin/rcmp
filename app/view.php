3e591349ff3e5ba6679dfd59acf106e459134632
nekrasov <nekrasov@mlsco.ru>
2014-04-30 18:44:50 +0600

    Перенес теплейты выше.

--- app/view.php
+++ app/view.php
@@ -13,10 +13,10 @@ class view{
   }
 
   public function render(){
-    $template_dir = ROOT.'app/templates/'.$this->path[1].'/';
+    $template_dir = ROOT.'templates/'.$this->path[1].'/';
     require_once ROOT.'libs/Twig/Autoloader.php';
     \Twig_Autoloader::register();
-    $loader = new \Twig_Loader_Filesystem(ROOT.'app/templates/');
+    $loader = new \Twig_Loader_Filesystem(ROOT.'templates/');
     $loader->prependPath($template_dir, $this->path[1]);
     $twig = new \Twig_Environment($loader);
     print $twig->render('@'.$this->path[1].'/'.$this->path[3].'.tpl', $this->content);
