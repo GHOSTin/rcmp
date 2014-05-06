533efedb175d4f4d3e798c25f22692631898a432
nekrasov <nekrasov@mlsco.ru>
2014-05-06 15:38:08 +0600

    Сделал правильно 404 страничку.

--- app/errors/controllers/error404.php
+++ app/errors/controllers/error404.php
@@ -1,13 +1,13 @@
 <?php namespace app\errors\controllers;
 
+use \boxxy\di;
 use \boxxy\classes\controller;
 use \boxxy\interfaces\request;
 
 class error404 extends controller{
 
   public function execute(request $request){
-    header("HTTP/1.0 404 Not Found");
-    die('404');
-    exit();
+    $php = di::get('\app\php');
+    $php->header("HTTP/1.0 404 Not Found");
   }
 }
