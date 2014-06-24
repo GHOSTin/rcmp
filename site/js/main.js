426beb2795dce214f78a59425858ec7fa331b1d2
nekrasov <nekrasov@mlsco.ru>
2014-06-24 19:39:35 +0600

    Сделал вставку картинки при онлайне.

--- site/js/main.js
+++ site/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         success: function(request){
             if($.trim(request) == 'online'){
                 $('#main-menu-header').append('<a class="navbar-brand" href="/stream" ><p class="label label-danger">Онлайн вещание</p></a>');
-                $('.page-header').prepend('<img src="http://pirate.rcmp.me/status/bg_v2.png" class="img-responsive img-online">');
+                $('.page-header').prepend('<img src="/images/online.png" class="img-responsive img-online">');
             }
         }
     });
