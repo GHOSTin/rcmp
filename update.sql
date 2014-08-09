bafee69675fe4579e999c7f32681fe9f05e5a4bb
GHOST home <lwhiteghostl@gmail.com>
2014-08-09 12:58:29 +0600

    добавлен sql файл для обновлений базы

---
+++ update.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `news` (
+`id` int(11) NOT NULL,
+  `user_id` smallint(5) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `pubtime` int(11) NOT NULL,
+  `description` text,
+  `rating` int(4) DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `news2votes` (
+  `news_id` int(8) NOT NULL,
+  `user_id` smallint(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
