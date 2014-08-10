fdc157c459ffcfece7330ac31698bb6cb28c2d9a
GHOST home <lwhiteghostl@gmail.com>
2014-08-11 00:11:47 +0600

    поправил sql

--- update.sql
+++ update.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS `news` (
-`id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` smallint(5) NOT NULL,
   `title` varchar(255) NOT NULL,
   `pubtime` int(11) NOT NULL,
   `description` text,
-  `rating` int(4) DEFAULT NULL
+  `rating` int(4) DEFAULT NULL,
+  PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 CREATE TABLE IF NOT EXISTS `news2votes` (
   `news_id` int(8) NOT NULL,
