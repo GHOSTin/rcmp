4c7eca0021f366b13db7c40193651593fb7202df
GHOST home <lwhiteghostl@gmail.com>
2014-08-18 17:03:12 +0600

    поправил sql для более четкого определения статуса новостей

--- app/news/mapper.php
+++ app/news/mapper.php
@@ -11,10 +11,12 @@ class mapper extends mapper_pdo {
    WHERE id = :id";
 
   private static $actual_news = "SELECT * FROM news
-   WHERE TIMESTAMPDIFF(DAY, FROM_UNIXTIME(`pubtime`, '%Y-%m-%d 00:00:00'), NOW()) <= MOD(3+WEEKDAY(NOW()), 7) ORDER BY pubtime";
+   WHERE TIMESTAMPDIFF(DAY, FROM_UNIXTIME(`pubtime`, '%Y-%m-%d 00:00:00'), CURDATE())
+    <= MOD(3+WEEKDAY(NOW()), 7) ORDER BY pubtime";
 
   private static $history_news = "SELECT * FROM news
-   WHERE TIMESTAMPDIFF(DAY, FROM_UNIXTIME(`pubtime`, '%Y-%m-%d 00:00:00'), NOW()) > MOD(3+WEEKDAY(NOW()), 7) ORDER BY pubtime";
+   WHERE TIMESTAMPDIFF(DAY, FROM_UNIXTIME(`pubtime`, '%Y-%m-%d 00:00:00'), CURDATE())
+   > MOD(3+WEEKDAY(NOW()), 7) ORDER BY pubtime";
 
   private static $id = "SELECT id FROM news ORDER BY id DESC LIMIT 1";
 
