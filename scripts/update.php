4ff9b3a78661be28ee048e82b4d3e5fc1e9087e4
GHOST home <lwhiteghostl@gmail.com>
2014-09-01 00:34:42 +0600

    обновлен update

--- scripts/update.php
+++ scripts/update.php
@@ -8,4 +8,12 @@ $pdo = new PDO('mysql:host='.conf::db_host.';dbname='.conf::db_name,
     conf::db_user, conf::db_password);
 $pdo->beginTransaction();
 $pdo->exec("ALTER TABLE `users` ADD `roles` TEXT NULL DEFAULT NULL");
+$pdo->exec(
+  "CREATE TABLE IF NOT EXISTS `podcasts` (
+  `time` bigint(16) NOT NULL,
+  `name` text NOT NULL,
+  `alias` text NOT NULL,
+  `url` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+);
 $pdo->commit();
