95612d340e4a11ee61bcd4797f3cc041d6811122
nekrasov <nekrasov@mlsco.ru>
2014-11-01 17:59:38 +0500

    Переход автозагрузку через композер.

--- site/index.php
+++ site/index.php
@@ -1,5 +1,4 @@
 <?php
 $root = substr(__DIR__, 0, (strlen(__DIR__) - strlen('/site'))).DIRECTORY_SEPARATOR;
 require_once($root."vendor/autoload.php");
-\boxxy\autoload::path($root);
 (new \boxxy\classes\container($root, new \app\app()))->run();
